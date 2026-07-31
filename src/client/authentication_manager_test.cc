c9bdfd75548e71f2e3ba27113a82bd3f29dcd45b
xiedeacc <xiedeacc@gmail.com>
2026-07-31 17:02:00 +0800

    Update authentication tests for key challenge

--- src/client/authentication_manager_test.cc
+++ src/client/authentication_manager_test.cc
@@ -5,6 +5,9 @@
 
 #include "src/client/authentication_manager.h"
 
+#include <chrono>
+#include <filesystem>
+#include <fstream>
 #include <memory>
 #include <thread>
 
@@ -18,6 +21,8 @@
 #pragma GCC diagnostic pop
 #endif
 #include "gtest/gtest.h"
+#include "src/common/ssh_key_auth.h"
+#include "src/impl/config_manager.h"
 #include "src/proto/service.grpc.pb.h"
 #include "src/proto/service.pb.h"
 
@@ -28,10 +33,14 @@ namespace {
 // Mock gRPC service implementation for testing.
 class MockTBOXServiceImpl final : public tbox::proto::TBOXService::Service {
  public:
-  MockTBOXServiceImpl()
+  MockTBOXServiceImpl(std::string private_key_path,
+                      std::string public_key_path)
       : should_succeed_(true),
+        server_error_(tbox::proto::User_login_error),
         return_token_("test_token"),
-        grpc_error_code_(grpc::StatusCode::OK) {}
+        grpc_error_code_(grpc::StatusCode::OK),
+        private_key_path_(std::move(private_key_path)),
+        public_key_path_(std::move(public_key_path)) {}
 
   void SetShouldSucceed(bool should_succeed) {
     should_succeed_ = should_succeed;
@@ -44,17 +53,45 @@ class MockTBOXServiceImpl final : public tbox::proto::TBOXService::Service {
   }
 
   grpc::Status UserOp(grpc::ServerContext*,
-                      const tbox::proto::UserRequest*,
+                      const tbox::proto::UserRequest* request,
                       tbox::proto::UserResponse* response) override {
     if (grpc_error_code_ != grpc::StatusCode::OK) {
       return grpc::Status(grpc_error_code_, grpc_error_msg_);
     }
 
-    if (should_succeed_) {
-      response->set_err_code(tbox::proto::ErrCode::Success);
+    if (!should_succeed_) {
+      response->set_err_code(server_error_);
+      return grpc::Status::OK;
+    }
+
+    response->set_err_code(tbox::proto::ErrCode::Success);
+    if (request->op() == tbox::proto::OP_USER_LOGIN_CHALLENGE) {
+      constexpr char kChallengeId[] = "authentication-test-challenge";
+      const std::string server_nonce(32, 'S');
+      const std::string message = tbox::common::SshKeyAuth::BuildLoginMessage(
+          kChallengeId, request->client_nonce(), server_nonce, request->user(),
+          request->request_id());
+      std::string server_signature;
+      if (!tbox::common::SshKeyAuth::CreateServerProof(
+              private_key_path_, public_key_path_, message,
+              &server_signature)) {
+        return grpc::Status(grpc::StatusCode::INTERNAL,
+                            "failed to create test server proof");
+      }
+
+      response->set_challenge_id(kChallengeId);
+      response->set_server_nonce(server_nonce);
+      response->set_server_signature(server_signature);
+      const auto expires_at =
+          std::chrono::duration_cast<std::chrono::seconds>(
+              std::chrono::system_clock::now().time_since_epoch())
+              .count() +
+          60;
+      response->set_challenge_expires_at(expires_at);
+    } else if (request->op() == tbox::proto::OP_USER_LOGIN) {
       response->set_token(return_token_);
     } else {
-      response->set_err_code(server_error_);
+      response->set_err_code(tbox::proto::Unsupported_op);
     }
 
     return grpc::Status::OK;
@@ -79,13 +116,49 @@ class MockTBOXServiceImpl final : public tbox::proto::TBOXService::Service {
   std::string return_token_;
   grpc::StatusCode grpc_error_code_;
   std::string grpc_error_msg_;
+  std::string private_key_path_;
+  std::string public_key_path_;
 };
 
 class AuthenticationManagerTest : public ::testing::Test {
  protected:
   void SetUp() override {
+    const auto test_dir = std::filesystem::current_path();
+    private_key_path_ =
+        test_dir / "authentication_manager_test_id_ed25519";
+    public_key_path_ =
+        test_dir / "authentication_manager_test_id_ed25519.pub";
+    config_path_ = test_dir / "authentication_manager_test_config.json";
+
+    {
+      std::ofstream private_key(private_key_path_, std::ios::binary);
+      private_key << "test-private-key-material\n";
+    }
+    {
+      std::ofstream public_key(public_key_path_, std::ios::binary);
+      public_key
+          << "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAITestKey authentication-test\n";
+    }
+    {
+      std::ofstream config(config_path_, std::ios::binary);
+      config << "{\n"
+             << "  \"server_addr\": \"http://127.0.0.1\",\n"
+             << "  \"grpc_server_port\": 1,\n"
+             << "  \"user\": \"test-user\",\n"
+             << "  \"password\": \"test-password\",\n"
+             << "  \"client_id\": \"test-client\",\n"
+             << "  \"ssh_private_key_path\": \""
+             << private_key_path_.generic_string() << "\",\n"
+             << "  \"ssh_public_key_path\": \""
+             << public_key_path_.generic_string() << "\"\n"
+             << "}\n";
+    }
+    ASSERT_TRUE(tbox::util::ConfigManager::Instance()->Init(
+        config_path_.string()));
+
     // Start mock gRPC server
-    mock_service_ = std::make_unique<MockTBOXServiceImpl>();
+    mock_service_ = std::make_unique<MockTBOXServiceImpl>(
+        private_key_path_.string(), public_key_path_.string());
 
     std::string server_address =
         "127.0.0.1:0";  // Use port 0 to get any available port
@@ -118,12 +191,19 @@ class AuthenticationManagerTest : public ::testing::Test {
       server_->Shutdown();
       server_->Wait();
     }
+    std::error_code error;
+    std::filesystem::remove(config_path_, error);
+    std::filesystem::remove(private_key_path_, error);
+    std::filesystem::remove(public_key_path_, error);
   }
 
   std::unique_ptr<MockTBOXServiceImpl> mock_service_;
   std::unique_ptr<grpc::Server> server_;
   std::shared_ptr<tbox::proto::TBOXService::Stub> stub_;
   std::shared_ptr<AuthenticationManager> auth_manager_;
+  std::filesystem::path config_path_;
+  std::filesystem::path private_key_path_;
+  std::filesystem::path public_key_path_;
   int selected_port_ = 0;
 };
 
